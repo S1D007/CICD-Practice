85d88c3a21895f9b22ba3a22aeea0a707332b855
Siddhant <94226898+S1D007@users.noreply.github.com>
2023-10-07 21:22:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/todo', function (req, res) {
         // Escapes HTML special characters in attribute values as HTML entities
         let newTodo = sanitizer.escape(req.body.newtodo);
         if (req.body.newtodo != '') {
-            todolist.push(newTodo);
+            todolist.push(`${newTodo} + Jenkins`);
         }
         res.redirect('/todo');
     })
